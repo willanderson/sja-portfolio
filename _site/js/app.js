e20050fe26c2e92c0a0dd9e52c7fd0ff94c71cea
Will Anderson <wmaxand@gmail.com>
2018-01-28 16:43:18 -0800

    Added infinite scroll
    Also removed masrony layout…for now…

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,22 +1,9 @@
 $(document).ready(function() {
-
-    var macy = Macy({
-        container: '.grid',
-        trueOrder: false,
-        waitForImages: false,
-        margin: 16,
-        columns: 3,
-        breakAt: {
-            940: 2,
-            540: 1
-        }
-    });
-
     $('.grid').infiniteScroll({
-        path: '.pagination__next',
-        append: '.post',
-        status: '.scroller-status',
-        scrollThresold: 100
+      path: '.pagination__next',
+      append: '.grid',
+      status: '.scroller-status',
+      scrollThresold: 1000
     });
 
 });
